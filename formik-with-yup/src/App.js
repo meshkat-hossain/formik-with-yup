9423b2b0396388e0c0c210dcdaa367f65c6d044f
Meshkat-Hossain <meshkat.hossain.himu@gmail.com>
2023-01-23 11:13:22 +0600

    form reate

---
+++ formik-with-yup/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Form validation With formik and yup</h1>
+
+     <form>
+      <label for="name">Name</label>
+      <input 
+      id="name" 
+      placeholder=" Type Your Name "
+      
+      />
+     </form>
+    </div>
+  );
+}
+
+export default App;
