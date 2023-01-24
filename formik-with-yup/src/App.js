b0b42fd094b680675867387f1e67061c98699bf5
Meshkat-Hossain <meshkat.hossain.himu@gmail.com>
2023-01-24 10:56:24 +0600

    validation

--- formik-with-yup/src/App.js
+++ formik-with-yup/src/App.js
@@ -1,19 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import FormikForm from './component/Form.jsx'
 
 function App() {
   return (
     <div className="App">
      <h1>Form validation With formik and yup</h1>
 
-     <form>
-      <label for="name">Name</label>
-      <input 
-      id="name" 
-      placeholder=" Type Your Name "
-      
-      />
-     </form>
+    <FormikForm />
     </div>
   );
 }
