6650e9f07b331b75f8f560391f9e6a25ea7a816a
Meshkat-Hossain <meshkat.hossain.himu@gmail.com>
2023-01-25 14:40:51 +0600

    delete old form

--- formik-with-yup/src/App.js
+++ formik-with-yup/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import FormikForm from './component/Form.jsx'
+import FormikContainer from './component/FormikContainer';
 
 function App() {
   return (
     <div className="App">
      <h1>Form validation With formik and yup</h1>
 
-    <FormikForm />
+    <FormikContainer />
     </div>
   );
 }
---
+++ formik-with-yup/src/component/FormikContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const FormikContainer = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default FormikContainer
+
+
+//formik and yup add
+//initial value declier
+//validation schema declear
+// value near jonne on submit 
+//formik 
+//formik er vhitore form use korte hobe.
+//formik er 3 props use korte hobe 
+//
